eda86e05d7fc58a8f7d12769e5a6bb4bc588c410
avideb24 <debavi24@gmail.com>
2025-08-01 12:39:26 +0600

    added favicon | moved global.css at styles directory

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import Navbar from "@/components/layout/navbar";
 import { QueryProvider } from "@/providers/query-provider";
